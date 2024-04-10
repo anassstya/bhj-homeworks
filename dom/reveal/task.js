a6681dd1d92420768cf56ff5dea0b9cc1c9cbf40
anassstya <408183@edu.itmo.ru>
2024-04-10 10:34:48 +0300

    done

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -4,9 +4,11 @@ const body =document.querySelector('body')
 function visible(){
   reveal.forEach((el) =>{
     const {top, bottom} = el.getBoundingClientRect();
-    if (bottom > 0){
-      el.classList.toggle('reveal_active');
-    } 
+    if (bottom > 0 && top > 0){
+      el.classList.add('reveal_active');
+    } else {
+      el.classList.remove('reveal_active');
+    }
   })
 }
 body.onscroll = visible;
