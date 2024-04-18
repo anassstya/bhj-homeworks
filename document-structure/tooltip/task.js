c63d598cd1b87f29b6791032b26c2cc14ae0cfb2
anassstya <408183@edu.itmo.ru>
2024-04-18 14:42:40 +0300

    done

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,28 @@
 const tips = [...document.querySelectorAll('.has-tooltip')];
-const activeTip = null;
+let activeTip = null;
 const tipText = document.createElement('div');
 tipText.classList.add('tooltip');
 
-tips.forEach(el => {
+tips.forEach((el, index) => {
   el.addEventListener('click', (event) => {
     event.preventDefault();
+
+    const check= activeTip && activeTip.previousElementSibling === el;
+
+    if (check) {
+      activeTip.classList.toggle('tooltip_active');
+      return; 
+    }
+
     tipText.innerHTML = el.title;
-    tipText.classList.add('tooltip_active');
+
     const parent = el.parentElement;
     parent.appendChild(tipText);
+    
+    const { top, bottom, left, right } = el.getBoundingClientRect();
+    const position = el.getAttribute('data-position');
 
-    const {top, bottom, left, right} = el.getBoundingClientRect();
-
-    const position = el.getAttribute('data-position')
+    
     if (position === 'right') {
       tipText.style.left = `${right}px`;
       tipText.style.top = `${top}px`;
@@ -24,11 +33,15 @@ tips.forEach(el => {
     }
     if (position === 'top') {
       tipText.style.left = `${left}px`;
-      tipText.style.top = `${top - tipText.offsetHeight}px`; 
+      tipText.style.top = `${top - tipText.offsetHeight}px`;
     }
     if (position === 'bottom') {
       tipText.style.left = `${left}px`;
       tipText.style.top = `${bottom}px`;
     }
-  })
-})
+
+    tipText.classList.add('tooltip_active');
+
+    activeTip = tipText;
+  });
+});
